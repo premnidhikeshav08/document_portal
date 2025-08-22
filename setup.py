b4b40af928abcce51ecf54ac4c5696e2a0ed8592
premnidhikeshav08 <prem.nidhikeshav08@gmail.com>
2025-08-22 23:30:54 +0530

    First Commit Added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='document_portal',  # Replace with your package name
+    version='0.1.0',  # Replace with your package version
+    author='Prem Nidhi Keshav',  # Replace with your name
+    packages=find_packages(),  # Automatically find packages in the current directory
+)
+    
